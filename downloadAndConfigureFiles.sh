ba48bccc9c02e3dd393de560cc209ebe08bc1b7e
joshwills <joshewills@gmail.com>
2020-07-28 15:54:48 -0400

    Update downloadAndConfigureFiles.sh

--- downloadAndConfigureFiles.sh
+++ downloadAndConfigureFiles.sh
@@ -1,3 +1,8 @@
+# 7/28/2020 a quick script to download and install the relevant haproxy configuration files for redirecting external access to the Duet.  Can be run with the following line:
+# wget https://raw.githubusercontent.com/joshwills/haproxy-duet-access/master/downloadAndConfigureFiles.sh && sudo bash downloadAndConfigureFiles.sh
+# Yes, this is very brittle, and yes, this is technically probably not the best way to go about performing administrative actions like this.  That's it - no excuses, just stating that I'm aware of some of the risks and issues, but doing it anyway.
+# Potential improvements - check that we're on a Pi image and configuration that we expect; the "sudo service haproxy stop" helps a bit, as it will prevent further actions if haproxy isn't installed or can't be stopped for some reason.
+
 wget https://raw.githubusercontent.com/joshwills/haproxy-duet-access/master/haproxy.cfg && \
 wget https://raw.githubusercontent.com/joshwills/haproxy-duet-access/master/503-no-duet.http && \
 sudo service haproxy stop && \
